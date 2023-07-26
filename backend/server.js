46216db4ad986f7bdc47a749e93061c6154deaea
Nick Karch <nickkarch@Nicks-MacBook-Pro.local>
2023-07-26 01:16:42 -0400

    Configure static dir, update gitignore with static dir

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
+import * as url from "url";
 
 import releaseRoutes from "./routes/releaseRoutes.js";
 
@@ -10,14 +11,17 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+
 connectDB();
 
 app.use(cors());
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/releases/", releaseRoutes);
+
+app.use(express.static(__dirname + "/static"));
 
 app.get("/", (req, res) => res.send("Server is ready"));
 
